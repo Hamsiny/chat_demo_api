66d9db8279616e2f2dfb1692c00861e0333b5d58
Ubuntu <azureuser@hamsiny-vm.ntzvdd2gxcbe3pghkfqmf03mmg.px.internal.cloudapp.net>
2023-04-13 00:50:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+	user: 'hamsiny',
+	host: 'chatdemodatabase.postgres.database.azure.com',
+	database: 'postgres',
+	password: 'Hjppvurtne1',
+	port: 5432,
+	ssl: true
+});
+
+pool.query('SELECT * FROM test_table', (err, res) => {
+	if (err) {
+		console.error(err);
+		return;
+	}
+	console.log(res.rows);
+});
